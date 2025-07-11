7c001618df1303840f6a9ebf7cfc0c85581d9e05
Abhilash A K <mailto.abhilashak@gmail.com>
2025-07-12 00:14:10 +0530

    Add React integration with working JSX support
    - Install React 19.1.0 and React DOM
    - Configure esbuild with JSX automatic transformation
    - Create HomeController and routes for React app
    - Add React components directory structure
    - Update README with professional project documentation
    - Set up development workflow with ./bin/dev
    
    React app now renders successfully

---
+++ app/javascript/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>React is working fine!</h1>
+      <p>Welcome to Rails + React App</p>
+    </div>
+  );
+}
+
+export default App;
